d00ae42d06d92f7dd180b59ba43b3570a55e6dcc
Charles Kurt <32605261+ckc64@users.noreply.github.com>
2020-05-01 21:09:06 +0800

    added player scripts and capsule

--- TopDownProto/Assets/Scripts/Player.cs
+++ TopDownProto/Assets/Scripts/Player.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerController))]
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public float moveSpeed = 5;
+
+    Camera viewCamera;
+    PlayerController playerController;
     void Start()
     {
-        
+        playerController = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        playerController.Move(moveVelocity);
+
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if(groundPlane.Raycast(ray,out rayDistance))
+        {
+           Vector3 point = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin, point, Color.red);
+            playerController.LookAt(point);
+        }
     }
 }
--- TopDownProto/Assets/Scripts/PlayerController.cs
+++ TopDownProto/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector3 velocity;
+    Rigidbody rbody;
     void Start()
     {
-        
+        rbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(Vector3 vel)
     {
-        
+        velocity = vel;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 correctedHeightPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(correctedHeightPoint);
+    }
+
+    public void FixedUpdate()
+    {
+        rbody.MovePosition(rbody.position + velocity * Time.fixedDeltaTime);
     }
 }
